5306cd80b03cc5db12e63a64b3f36f99cafb97d7
Denis Pribavkin <pribavkin@haulmont.com>
2019-05-23 16:20:38 +0400

    Product entity created

--- modules/core/db/init/hsql/10.create-db.sql
+++ modules/core/db/init/hsql/10.create-db.sql
@@ -1,1 +1,17 @@
-
+-- begin DEMO_PRODUCT
+create table DEMO_PRODUCT (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NAME varchar(255),
+    PRICE decimal(19, 2),
+    --
+    primary key (ID)
+)^
+-- end DEMO_PRODUCT
---
+++ modules/global/src/com/company/demo/entity/Product.java
@@ -0,0 +1,36 @@
+package com.company.demo.entity;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@NamePattern("%s|name")
+@Table(name = "DEMO_PRODUCT")
+@Entity(name = "demo_Product")
+public class Product extends StandardEntity {
+    @Column(name = "NAME")
+    protected String name;
+
+    @Column(name = "PRICE")
+    protected BigDecimal price;
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
